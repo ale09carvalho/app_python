f691d9fbf74cc256f72af48e96a37a01505dea22
ale09carvalho <ale09.carvalho@gmail.com>
2025-05-13 21:30:18 -0300

    atividade do dia 13/05 - incluir  anotaçao funçoes05

---
+++ funcoes05/anotacao.py
@@ -0,0 +1,10 @@
+'''
+sera criada um arquivo __pycache__
+para ignorar este arquivo , podera criar um arquivo no mesmo diretorio com nome ----> .gitignore ---
+detro do arquivo os seguintes comandos:
+__pycache__
+.gitignore
+
+ou se for fora do diretorio o seguinte comando
+exemplo : funcoes05/__pycache__
+'''
