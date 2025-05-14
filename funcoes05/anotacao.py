00c4050b0925e9f7349669b28ff5b255d58fe8e3
ale09carvalho <ale09.carvalho@gmail.com>
2025-05-13 21:34:13 -0300

    alterar arquivo  anotaçao funçoes05

--- funcoes05/anotacao.py
+++ funcoes05/anotacao.py
@@ -1,6 +1,13 @@
 '''
 sera criada um arquivo __pycache__
-para ignorar este arquivo , podera criar um arquivo no mesmo diretorio com nome ----> .gitignore ---
+funciona da seguinte maneira, quando você executa um script em Python, o interpretador o compila para versão bytecode pyc e após isso guarda no diretório __pycache__.
+Boa prática
+Quando você for compartilhar o seu código com outras pessoas, a prática comum é deletar esse diretório. 
+Caso, você suba o seu código através de um controle de versão git, é necessário listar o __pycache__ no arquivo .gitignore, 
+para que a pasta não seja incluída no git push
+
+
+Para ignorar este arquivo , podera criar um arquivo no mesmo diretorio com nome ----> .gitignore ---
 detro do arquivo os seguintes comandos:
 __pycache__
 .gitignore
